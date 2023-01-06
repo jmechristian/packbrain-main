91b62482d41fc7f381d0ac7606dea10e4521d55c
jmechristian <jmechristian@gmail.com>
2023-01-05 20:43:50 -0500

    ssss

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App({ signOut, user }) {
   return (
     <>
       <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign outf</button>
     </>
   );
 }
